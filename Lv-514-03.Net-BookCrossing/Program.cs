8bf3a894095a044b94fa2f638b65a2510287df0d
Denbuvalyy <57112814+Denbuvalyy@users.noreply.github.com>
2020-11-02 00:14:49 +0200

    Add project files.

---
+++ Lv-514-03.Net-BookCrossing/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lv_514_03.Net_BookCrossing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
